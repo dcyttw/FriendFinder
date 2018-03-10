31d0e021e71c602baa9fd4de5b79f911e9a25c64
Dennis Chi <dcyttw@gmail.com>
2018-03-09 23:43:12 -0600

    HTML Files & API Route Commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -33,9 +33,10 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
-  app.post("/api/clear", function() {
+  app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
     friends = [];
     console.log(friends);
+    res.json(true);
   });
 };
